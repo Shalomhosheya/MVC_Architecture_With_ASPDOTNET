2fbc6258aa6d3d397a163dd4cf8ea07e92095c80
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-11-14 00:06:35 +0530

    files add

--- WebApplicationORM/Program.cs
+++ WebApplicationORM/Program.cs
@@ -1,8 +1,24 @@
 using MySqlConnector;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddSingleton(new MySqlConnection(connectionString));
 
-var app =  builder.Build();
+// ✅ Database Connection
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddTransient<MySqlConnection>(_ => new MySqlConnection(connectionString));
+
+// ✅ Enable MVC (Controllers + Views)
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// ✅ Middleware
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=User}/{action=Index}/{id?}"
+);
+
 app.Run();
--- WebApplicationORM/obj/Debug/net9.0/WebApplicationORM.AssemblyInfo.cs
+++ WebApplicationORM/obj/Debug/net9.0/WebApplicationORM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplicationORM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed3271ea71c0052e60c28fcdda327f327c0c23a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7176b2fe84a4980003719fe7de2cbf13363c3976")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationORM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationORM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
